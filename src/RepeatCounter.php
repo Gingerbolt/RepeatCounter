2fe3f2fd388e610cf483bf34ce44dcca4bfd999b
Steve Spitz <steve@>
2017-07-13 07:54:26 -0700

    Refactor main filter function to no longer utilise a regular expression; Tests successful; Add spaces for improved readability of countRepeats;

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,21 +4,21 @@
         function countRepeats($input_term, $input_target)
         {
             $count = 0;
-            $removal_targets = [",", ".", "<", ">", "?", "/", ";", ":", "{", "}", "[", "]", "|", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_", "+", "="];
-            $input_target_filtered = strtoupper($input_target);
-            $input_term_filtered = strtoupper($input_term);
-            $target_split_array = str_split($input_target_filtered);
-            foreach ($target_split_array as $target_piece)
-            {
-                for($x = 0; $x <= 37; $x++) {
-                preg_replace('/$removal_targets[$x]/', '', $target_piece);
-                }
-            }
-            $input_target_distilled = implode($target_split_array);
+            $empty_array = array('');
+            $removal_targets = array(",", ".", "<", ">", "?", "/", ";", ":", "{", "}", "[", "]", "|", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_", "+", "=");
+            
+            $input_target_capped = strtoupper($input_target);
+            $input_term_capped = strtoupper($input_term);
+            $target_split_array = str_split($input_target_capped);
+
+            $target_split_array_filtered = str_ireplace($removal_targets, $empty_array, $target_split_array);
+
+            $input_target_distilled = implode($target_split_array_filtered);
+
             $target_array = explode(' ', $input_target_distilled);
             foreach ($target_array as $target)
             {
-                if ($target == $input_term_filtered) {
+                if ($target == $input_term_capped) {
                     ++$count;
                 }
             }
